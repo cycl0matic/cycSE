9a5b0ab638cb738dcd6b12166de95f336821799e
Cycl0matic <muhammadalaidy@protonmail.com>
2020-11-07 09:44:33 +0200

    add search mechanism

---
+++ Search.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include "Trienode.h"
+#include "Map.h"
+using namespace std;
+
+
+void search(char* token, Trienode *trie, Mymap *map, int k);
+void df(Trienode* trie);
+int tf(char* token, Trienode* trie);
